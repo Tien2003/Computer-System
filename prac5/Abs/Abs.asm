3884361e4067f2cd0633caceee74c54f9e88a3c2
Tien <autien0809@gmail.com>
2024-09-15 17:01:19 +0930

    abs

--- prac5/Abs/Abs.asm
+++ prac5/Abs/Abs.asm
@@ -2,3 +2,22 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+@1
+D=M
+@END
+D;JGT
+
+(WHILE)
+    @1
+    D=-M
+    @0
+    M=D
+    @WHILE
+    D;JGE
+(END)
+    @1
+    D=M
+    @0
+    M=D
+    @END
+    M;JGT
